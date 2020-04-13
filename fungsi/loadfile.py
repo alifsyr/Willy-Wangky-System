516d4a3240f976f4781be378a29a98f1eb6584ca
VegetarianRacoon <igovinda100@gmail.com>
2020-04-13 21:14:29 +0700

    Who needs append anyways

--- fungsi/loadfile.py
+++ fungsi/loadfile.py
@@ -3,11 +3,6 @@ import csv
 def loadfile(x):
     with open(x) as csvfile :
         reader = csv.reader (csvfile, delimiter=',')
-        rownum = 0
-        data = []
-
-        for row in reader:
-            data.append (row)
-            rownum += 1
+        data = [row for row in reader]
     
     return data
