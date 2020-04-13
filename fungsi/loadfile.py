329d35704974f889c7a6a441d8382d15f0f805fa
preman.id <alifsyr@gmail.com>
2020-04-13 11:43:41 +0700

    Update loadfile.py

--- fungsi/loadfile.py
+++ fungsi/loadfile.py
@@ -1,47 +1,26 @@
 import csv
 
 def loadfile(x):
-    global user_data
-    global wahana_data
-    global tiket_data
-    global refund_data
-    global penggunaan_data
-    global pembelian_data
-    global kritiksaran_data
-    if x == "user.csv":
-        with open(x) as user :
-            user_data = csv.reader (user, delimiter=',')
+    with open(x) as csvfile :
+        reader = csv.reader (csvfile, delimiter=',')
+        rownum = 0
+        data = []
+
+        for row in reader:
+            data.append (row)
+            rownum += 1
+    
+    return data
+
             
-    elif x == "wahana.csv":
-        with open(x) as wahana :
-            wahana_data = csv.reader (wahana, delimiter=',')
             
-    elif x == "tiket.csv":
-        with open(x) as tiket :
-            tiket_data = csv.reader (tiket, delimiter=',')
-            
-    elif x == "refund.csv":
-        with open(x) as refund :
-            refund_data = csv.reader (refund, delimiter=',')
-                    
-    elif x == "penggunaan.csv":
-        with open(x) as penggunaan :
-            penggunaan_data = csv.reader (penggunaan, delimiter=',')
-            
-    elif x == "pembelian.csv":
-        with open(x) as pembelian :
-            pembelian_data = csv.reader (pembelian, delimiter=',')
-            
-    else :
-        with open(x) as kritiksaran :
-            kritiksaran_data = csv.reader (kritiksaran, delimiter=',')
-            
-loadfile(input('Masukan nama File User: ')) # user.csv
-loadfile(input('Masukan nama File Daftar Wahana: ')) # wahana.csv
-loadfile(input('Masukan nama File Pembelian Tiket: ')) # pembelian.csv
-loadfile(input('Masukan nama File Penggunaan Tiket: ')) # penggunaan.csv
-loadfile(input('Masukkan nama File Kepemilikan Tiket: ')) # tiket.csv
-loadfile(input('Masukkan nama File Refund Tiket: ')) # refund.csv
-loadfile(input('Masukkan nama File Kritik dan Saran: ')) #kritiksaran.csv
+user_data = loadfile(input('Masukan nama File User: ')) # user.csv
+print(user_data)
+wahana_data = loadfile(input('Masukan nama File Daftar Wahana: ')) # wahana.csv
+pembelian_data = loadfile(input('Masukan nama File Pembelian Tiket: ')) # pembelian.csv
+penggunaan_data = loadfile(input('Masukan nama File Penggunaan Tiket: ')) # penggunaan.csv
+tiket_data = loadfile(input('Masukkan nama File Kepemilikan Tiket: ')) # tiket.csv
+refund_data = loadfile(input('Masukkan nama File Refund Tiket: ')) # refund.csv
+kritiksaran_data = loadfile(input('Masukkan nama File Kritik dan Saran: ')) #kritiksaran.csv
 
 print("File perusahaan Willy Wangky’s Chocolate Factory telah di-load.")
