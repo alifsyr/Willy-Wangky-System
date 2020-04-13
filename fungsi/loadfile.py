a2393f61af8585f23bae1e0e2f9b4d6aa8f4e413
VegetarianRacoon <igovinda100@gmail.com>
2020-04-13 12:03:43 +0700

    Added main.py functionality

--- fungsi/loadfile.py
+++ fungsi/loadfile.py
@@ -11,16 +11,3 @@ def loadfile(x):
             rownum += 1
     
     return data
-
-            
-            
-user_data = loadfile(input('Masukan nama File User: ')) # user.csv
-print(user_data)
-wahana_data = loadfile(input('Masukan nama File Daftar Wahana: ')) # wahana.csv
-pembelian_data = loadfile(input('Masukan nama File Pembelian Tiket: ')) # pembelian.csv
-penggunaan_data = loadfile(input('Masukan nama File Penggunaan Tiket: ')) # penggunaan.csv
-tiket_data = loadfile(input('Masukkan nama File Kepemilikan Tiket: ')) # tiket.csv
-refund_data = loadfile(input('Masukkan nama File Refund Tiket: ')) # refund.csv
-kritiksaran_data = loadfile(input('Masukkan nama File Kritik dan Saran: ')) #kritiksaran.csv
-
-print("File perusahaan Willy Wangky’s Chocolate Factory telah di-load.")
