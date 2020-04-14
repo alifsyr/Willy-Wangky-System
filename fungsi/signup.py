adcc7415496f5d6ec2dfb03eb2d2db1ff74f317d
preman.id <alifsyr@gmail.com>
2020-04-14 13:36:49 +0700

    Update signup.py

--- fungsi/signup.py
+++ fungsi/signup.py
@@ -7,4 +7,4 @@ def signup():
         for row in user_data:
             if f'{row[0][5]}' == "Admin":
                 with open('user.csv',mode='a') as sign_up:
-                    print("STOP GIVING ME ERRORS")
+                    
