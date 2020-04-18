66d57b0e27f7156f4b4b7da04ff430981080544f
rayendito <56763816+rayendito@users.noreply.github.com>
2020-04-19 06:22:17 +0700

    Update login.py

--- fungsi/login.py
+++ fungsi/login.py
@@ -1,25 +1,20 @@
 def login(x):
 
-        success = False
-        unameValid = False
         paswValid = False
-        i = 0
-
-        while not(success):
+        
+        while not(paswValid):
                 uname = input('Masukkan username: ')
                 pasw = input('Masukkan password: ')
-
-                while i < len(x) and not(paswValid):
-                        if x[i][3] == uname:
-                                unameValid = True
-                                if(unameValid):
-                                        if x[i][4] == pasw:
-                                                paswValid = True
-                        else:
-                                i += 1
+                
+                for i in x:
+                        if i[3] != 'Username':
+                                if i[3] == uname:
+                                    if i[4] == pasw:
+                                        paswValid = True
+                                        nama = i[0]
+                    
                 if(paswValid):
-                        print("Selamat bersenang-senang, "+x[i][0])
+                        print("Selamat bersenang-senang, "+nama)
                         return(True) #untuk indikasi login berhasil
                 else:
                         print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi!")
-login('user.csv')
