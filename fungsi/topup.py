7b6ec1bd7147e0cc09375b2e96c8a2e7b3f568cb
rayendito <56763816+rayendito@users.noreply.github.com>
2020-04-19 07:04:07 +0700

    Create topup.py

---
+++ fungsi/topup.py
@@ -0,0 +1,11 @@
+def topup(x):
+    uname = input('Masukkan username: ')
+    tambah = int(input('Masukkan saldo yang di-top up: '))
+    for i in x:
+        if i[3] != 'Username':
+            if i[3] == uname:
+                nama = i[0]
+                i[6] = i[6] + tambah
+                saldo = i[6]
+    print()
+    print("Top up berhasil. Saldo "+nama+" bertambah menjadi "+str(saldo))
