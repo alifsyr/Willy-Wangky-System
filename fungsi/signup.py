7298c19e4572f9b6ea77d5af4f4a2bfab05fd898
preman.id <alifsyr@gmail.com>
2020-04-22 23:12:59 +0700

    Update signup.py

--- fungsi/signup.py
+++ fungsi/signup.py
@@ -1,20 +1,19 @@
 import csv
 
 def signup(x):
-    success = False
     unameAvailable = False
-    global new_data
     global user_data
     new_data = [0 for i in range (7)]
     i = 0
-    while not(success):
+    while (role == "Admin"):
         for i in new_data:
             new_data[0]   = input("Masukkan nama pemain: ")
             new_data[1]   = input("Masukkan tanggal lahir pemain: ")
             new_data[2]   = input("Masukkan tinggi badan pemain (cm):  ")
             new_data[3]   = input("Masukkan username pemain: ")
-            while  i < panjang(user_data) and not(unameAvailable):
-                        if user_data[i][3] == new_data[3]:
+            while  i < panjang(x) and not(unameAvailable):
+                        if x[i][3] == new_data[3]:
+                            unameAvailable = True
                             print("Username sudah digunakan, silahkan masukan username lain.")
                             new_data[3]   = input("Masukkan username pemain: ")
                                
@@ -24,7 +23,7 @@ def signup(x):
             new_data[4]   = input("Masukkan password pemain: ")
             new_data[5]   = ("pemain")
             new_data[6]   = ("0")
+            print("Selamat menjadi pemain,",new_data[0],". Selamat bermain.")
             user_data = user_data + [new_data]
             return user_data
-        
-print("Selamat menjadi pemain,",new_data[0],". Selamat bermain.")
+    print("Maaf anda bukan Admin !")  
