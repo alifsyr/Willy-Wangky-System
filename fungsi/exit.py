6fa6512b2878407c0a39f73c3d321a472b9497ef
rayendito <56763816+rayendito@users.noreply.github.com>
2020-04-23 16:40:06 +0700

    Update exit.py

--- fungsi/exit.py
+++ fungsi/exit.py
@@ -1,6 +1,6 @@
 def exit():
     saev = input('Apakah anda mau melakukan penyimpanan file yang sudah dilakukan (Y/N) ?')
     if saev == 'Y' or saev == 'y':
-        return(user_data)   #array user_data yang habis diproses sama fungsi fungsi sebelumnya
-    elif saev == 'N' or saev 'n':
-        return(arrayConstant)   #array A yang dibuat konstan di awal file
+        return(True)   #return True berarti jalankan save
+    elif saev == 'N' or saev == 'n':
+        return(False)   #return false berarti jangan jalankan save
