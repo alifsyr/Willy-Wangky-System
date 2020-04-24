61aa03219e3df8fbd2830618c5b63dfc63ddc3be
rayendito <56763816+rayendito@users.noreply.github.com>
2020-04-24 20:12:33 +0700

    Update topup.py

--- fungsi/topup.py
+++ fungsi/topup.py
@@ -5,7 +5,15 @@ def topup(user_data):
         if i[6] != 'Saldo':
             if i[3] == uname:
                 nama = i[0]
-                i[6] = i[6] + tambah
+                i[6] = int(i[6]) + tambah
                 saldo = i[6]
     print()
     print("Top up berhasil. Saldo "+nama+" bertambah menjadi "+str(saldo))
+
+
+#ignore, ini buat ngetes
+aa = [['Nama','Tanggal_Lahir','Tinggi_Badan','Username','Password','Role','Saldo'],
+      ['Babi','12/12/2001',170,'babiman','XXXXXX','Admin',0],
+      ['Kucing','12/04/2020',45,'kucingman','sukasarden123','Admin',0],
+      ['Bebek','12/04/2020',45,'bebekman','kweks','Pemain',99]]
+
