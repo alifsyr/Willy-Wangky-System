23b484112574a8b6716f57b35fca003e5deb6310
rayendito <56763816+rayendito@users.noreply.github.com>
2020-04-24 20:20:58 +0700

    Update F05_caripemain.py

--- fungsi/F05_caripemain.py
+++ fungsi/F05_caripemain.py
@@ -1,14 +1,17 @@
-import csv
-import modules
+
 
 # type user : (Nama : string, Tinggi_Badan : string, Tanggal_Lahir : string, username : string)
 # data : SEQFILE of data_pemain
 # data_pemain : user
 def EOP(x) :
+    import csv
+    import modules
     return ( x[0] == mark[0] and x[1] == mark[1] and x[2] == mark[2] and x[3] == mark[3])
 
 
 def cari_pemain():
+    import csv
+    import modules
     username = input('Masukkan username: ')
     data_pemain = csv.reader(open('user.csv', "rb"), delimiter=",")
     found = False
