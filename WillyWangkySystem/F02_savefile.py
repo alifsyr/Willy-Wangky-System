1a36b7e1cd399f5fb5c5043389c7eaf4ffe801b2
VegetarianRacoon <igovinda100@gmail.com>
2020-04-24 20:28:18 +0700

    Update F02_savefile.py

--- WillyWangkySystem/F02_savefile.py
+++ WillyWangkySystem/F02_savefile.py
@@ -5,9 +5,9 @@ def save(data, names):
     import modules
     for i in range(modules.panjang(names)):
         name = str(input("Masukkan nama file ", names[i], ":", sep=""))
-        saveFile(name, data[i])
+        writeFile(name, data[i])
         
-def saveFile(namaFile, arrayData):
+def writeFile(namaFile, arrayData):
     datafile = open(namaFile, "w")
     writer = csv.writer(datafile, delimiter=',')
 
