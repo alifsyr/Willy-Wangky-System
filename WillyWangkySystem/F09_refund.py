06e839c95c01422cf07ea63bb42ed1d4192b87be
Alvin Rizqi Alfisyahrin <alvin.asfis03@gmail.com>
2020-04-25 00:56:10 +0700

    Update F09_refund.py

--- WillyWangkySystem/F09_refund.py
+++ WillyWangkySystem/F09_refund.py
@@ -15,7 +15,7 @@ def refund(currentUser, tiket_data, refund_data, wahana_data):
     username = currentUser[3]
     if id_wahana != tiket_data[username][1] or jumlah_refund > tiket_data[username][2]:
         print('Anda tidak memiliki tiket terkait')
-    elif id_wahana == tiket_data[username][1] or jumlah_refund <= tiket_data[username][2]
+    elif id_wahana == tiket_data[username][1] or jumlah_refund <= tiket_data[username][2]:
         print('Uang refund sudah kami berikan pada akun Anda.')
         newRefund = (str(currentUser[3]), str(tanggal), str(id_wahana), str(tiket_data[username][2]-jumlah_refund))
         newKepemilikan = (str(currentUser[3]), str(id_wahana), str(tiket_data[id_wahana][2] - jumlah_refund))
