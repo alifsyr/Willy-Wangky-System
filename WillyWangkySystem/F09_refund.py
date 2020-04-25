43471d53a273b8effa6d59b9c38f0d97dbdc0b21
Alvin Rizqi Alfisyahrin <alvin.asfis03@gmail.com>
2020-04-25 18:09:52 +0700

    update f09, udah ditesting dan bisa

--- WillyWangkySystem/F09_refund.py
+++ WillyWangkySystem/F09_refund.py
@@ -2,19 +2,26 @@ def refund(currentUser, tiket_data, refund_data, wahana_data):
     import modules
     '''
     currentUser : Nama, Tanggal Lahir, Tinggi, Username, Password, Role, Saldo
-    tiket_data : username, tanggal pembelian,id wahana, jumlah tiket
+    tiket_data : username, id wahana, jumlah tiket
     refund_data : username, tanggal refund, id wahana, jumlah tiket
     wahana_data : ID, Nama wahana, Harga tiket, Batasan umur, Batasan Tinggi
     '''
     id_wahana = input('Masukkan ID Wahana: ')
     tanggal = input('Masukkan tanggal refund: ')
-    jumlah_refund = input('Jumlah tiket yang di refund: ')
+    jumlah_refund = int(input('Jumlah tiket yang di refund: '))
     username = currentUser[3]
-    if id_wahana != tiket_data[username][1] or jumlah_refund > int(tiket_data[username][2]):
-        print('Anda tidak memiliki tiket terkait')
-    elif id_wahana == tiket_data[username][1] or jumlah_refund <= (tiket_data[username][2]):
-        print('Uang refund sudah kami berikan pada akun Anda.')
-        newRefund = (str(currentUser[3]), str(tanggal), str(id_wahana), str(tiket_data[username][2]-jumlah_refund))
-        newKepemilikan = (str(currentUser[3]), str(id_wahana), str(tiket_data[id_wahana][2] - jumlah_refund))
-        currentUser[6] = currentUser[6] - (((wahana_data[id_wahana][2])/10)*jumlah_refund)
-        return refund_data + newRefund, tiket_data + newKepemilikan
+    found = False
+    if currentUser[5] == 'Pemain' :
+        for row in tiket_data:
+            if row[0] == username:
+                if id_wahana == row[1] and jumlah_refund <= int(row[2]):
+                    found = True
+                    print('Uang refund sudah kami berikan pada akun Anda.')
+                    newRefund = [str(currentUser[3]), str(tanggal), str(id_wahana), str(jumlah_refund)]
+                    newKepemilikan = [str(currentUser[3]), str(id_wahana), str(int(row[2]) - jumlah_refund)]
+                    for i in wahana_data:
+                        if id_wahana == i[0]:
+                            currentUser[6] = str(int(currentUser[6]) + ((int(i[2])/10)*(jumlah_refund)))
+                    return refund_data + newRefund, tiket_data + newKepemilikan
+        if not(found) :
+            print('Tiket anda tidak valid dalam data kami')
