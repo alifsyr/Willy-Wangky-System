fffa8c2488e4bb81a9a543d1a651315548a4f8b5
Nafa-Mumtaz <57821989+Nafa-Mumtaz@users.noreply.github.com>
2020-04-25 12:30:21 +0700

    Update F10_givefeedback.py

--- WillyWangkySystem/F10_givefeedback.py
+++ WillyWangkySystem/F10_givefeedback.py
@@ -1,10 +1,13 @@
 import modules
 def giveFeedback(kritiksaran_data, currentUser):
-    ID_Wahana = input("Masukkan ID wahana: ")
-    Tanggal_Kritik = input("Masukkan tanggal pelaporan: ")
-    Isi_Kritik = input("Kritik/saran anda: ")
-    print()
-    FeedBack = [[str(currentUser[3]),  str(Tanggal_Kritik), str(ID_Wahana), str(Isi_Kritik)]]
-    print("Kritik dan saran anda kami terima")
-    kritiksaran_data = kritiksaran_data + FeedBack
+    if (currentUser[5] != "Pemain"):
+        print("Anda tidak mendapat izin untuk mengakses fungsi ini.")
+    else:
+        ID_Wahana = input("Masukkan ID wahana: ")
+        Tanggal_Kritik = input("Masukkan tanggal pelaporan: ")
+        Isi_Kritik = input("Kritik/saran anda: ")
+        print()
+        FeedBack = [[str(currentUser[3]),  str(Tanggal_Kritik), str(ID_Wahana), str(Isi_Kritik)]]
+        print("Kritik dan saran anda kami terima")
+        kritiksaran_data = kritiksaran_data + FeedBack
     return (kritiksaran_data)
