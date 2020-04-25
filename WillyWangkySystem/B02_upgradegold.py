a7fa9d5d24296bc6d2ccd3b7e116cd22f1628013
preman.id <alifsyr@gmail.com>
2020-04-25 23:18:16 +0700

    Update B02_upgradegold.py

--- WillyWangkySystem/B02_upgradegold.py
+++ WillyWangkySystem/B02_upgradegold.py
@@ -1,9 +1,13 @@
 import csv
 
-def upgrade_gold(x):    
+def upgrade_gold(user_data):    
     change_acc = input("Masukkan username yang ingin di-upgrade: ")
-    for i in x :
-        if i[3] == change_acc:
-            i[5] = ("Gold")
-        print("Akun Anda telah diupgrade.")
-    
+    for i in (user_data) :
+        if i[6] != "Saldo":
+            if int(i[6] >= 100000):
+                int(i[6]) - 100000
+                if i[3] == change_acc:
+                    i[5] = ("Gold")
+                    print("Akun Anda telah diupgrade.")
+            else:
+                print("Maaf saldo anda tidak cukup.")
