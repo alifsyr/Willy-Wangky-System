f84e5e77e84ebec01b088affef8a4c690556cd06
rayendito <56763816+rayendito@users.noreply.github.com>
2020-04-26 01:15:31 +0700

    Update main.py

--- WillyWangkySystem/main.py
+++ WillyWangkySystem/main.py
@@ -24,65 +24,71 @@ while (not endprogram):
         
     elif command == "autoload":
         user_data, wahana_data, pembelian_data, penggunaan_data, tiket_data, refund_data, kritiksaran_data, tikethilang_data = F01_loadfile.autoLoad()
-    
-    elif command == "save":
-        data = [user_data, wahana_data, pembelian_data, penggunaan_data, tiket_data, refund_data, kritiksaran_data, tikethilang_data]
-        names = ["User", "Daftar Wahana", "Pembelian Tiket", "Penggunaan Tiket", "Kepemilikan Tiket", "Refund Tiket", "Kritik dan Saran"]
-        F02_savefile.save(data, names)
 
-    elif command == "signup":
-        F03_signup.signup(user_data, currentUser)    
-    
     elif command == "login":
         currentUser = F04_login.login(user_data)
-    
-    elif command == "cari_pemain":
-        F05_caripemain.cari_pemain(currentUser, user_data)
-    
-    elif command == "cari":
-        F06_filterrides.filterRides(wahana_data)
-    
-    elif command == "beli_tiket":
-        pembelian_data, tiket_data, currentUser = F07_belitiket.beliTiket(pembelian_data, tiket_data, wahana_data, currentUser)
-        modules.updateArray(user_data, currentUser, currentUser[3], 3)
-
-    elif command == "main":
-        F08_penggunaan.use_ticket(penggunaan_data, tiket_data, currentUser)
-    
-    elif command == "refund":
-        F09_refund.refund(currentUser, tiket_data, refund_data, wahana_data)
-    
-    elif command == "kritik_saran":
-        kritiksaran_data = F10_givefeedback.giveFeedback(kritiksaran_data, currentUser)
-        
-    elif command == "lihat_laporan":
-        F11_accessfeedback.accessFeedback(kritiksaran_data)
-    
-    elif command == "tambah_wahana":
-        wahana_data = F12_addride.addRide(wahana_data, currentUser)
-    
-    elif command == "topup":
-        F13_topup.topup(user_data)
-    
-    elif command == "riwayat_wahana":
-        F14_ridehistory.rideHistory(penggunaan_data, currentUser)
-    
-    elif command == "tiket_pemain":
-        F15_ticketcount.showTicket(tiket_data, currentUser)
-
-    elif command == "exit":
-        simmpan = F16_exit.exit()
-        if (simmpan):
-            data = [user_data, wahana_data, pembelian_data, penggunaan_data, tiket_data, refund_data, kritiksaran_data, tikethilang_data]
-            names = ["User", "Daftar Wahana", "Pembelian Tiket", "Penggunaan Tiket", "Kepemilikan Tiket", "Refund Tiket", "Kritik dan Saran"]
-            F02_savefile.save(data, names)
-        endprogram = True
-    
-    elif command == "showdata":
-        data = [user_data, wahana_data, pembelian_data, penggunaan_data, tiket_data, refund_data, kritiksaran_data, tikethilang_data]
-        for i in data:
-            print(i, end=" | ")
-            print("\n")
 
     else:
-        print("Command tidak dikenali, gunakan \"help\" untuk bantuan")
+
+        if currentUser[6] == " $NOUSER" :
+            print("Silahkan login terlebih dahulu")
+
+        else:
+            if command == "save":
+                data = [user_data, wahana_data, pembelian_data, penggunaan_data, tiket_data, refund_data, kritiksaran_data, tikethilang_data]
+                names = ["User", "Daftar Wahana", "Pembelian Tiket", "Penggunaan Tiket", "Kepemilikan Tiket", "Refund Tiket", "Kritik dan Saran"]
+                F02_savefile.save(data, names)
+
+            elif command == "signup":
+                F03_signup.signup(user_data, currentUser)    
+    
+            elif command == "cari_pemain":
+                F05_caripemain.cari_pemain(currentUser, user_data)
+    
+            elif command == "cari":
+                F06_filterrides.filterRides(wahana_data)
+    
+            elif command == "beli_tiket":
+                pembelian_data, tiket_data, currentUser = F07_belitiket.beliTiket(pembelian_data, tiket_data, wahana_data, currentUser)
+                modules.updateArray(user_data, currentUser, currentUser[3], 3)
+
+            elif command == "main":
+                F08_penggunaan.use_ticket(penggunaan_data, tiket_data, currentUser)
+    
+            elif command == "refund":
+                F09_refund.refund(currentUser, tiket_data, refund_data, wahana_data)
+    
+            elif command == "kritik_saran":
+                kritiksaran_data = F10_givefeedback.giveFeedback(kritiksaran_data, currentUser)
+        
+            elif command == "lihat_laporan":
+                F11_accessfeedback.accessFeedback(kritiksaran_data)
+    
+            elif command == "tambah_wahana":
+                wahana_data = F12_addride.addRide(wahana_data, currentUser)
+    
+            elif command == "topup":
+                F13_topup.topup(user_data)
+    
+            elif command == "riwayat_wahana":
+                F14_ridehistory.rideHistory(penggunaan_data, currentUser)
+    
+            elif command == "tiket_pemain":
+                F15_ticketcount.showTicket(tiket_data, currentUser)
+
+            elif command == "exit":
+                simmpan = F16_exit.exit()
+                if (simmpan):
+                    data = [user_data, wahana_data, pembelian_data, penggunaan_data, tiket_data, refund_data, kritiksaran_data, tikethilang_data]
+                    names = ["User", "Daftar Wahana", "Pembelian Tiket", "Penggunaan Tiket", "Kepemilikan Tiket", "Refund Tiket", "Kritik dan Saran"]
+                    F02_savefile.save(data, names)
+                endprogram = True
+    
+            elif command == "showdata":
+                data = [user_data, wahana_data, pembelian_data, penggunaan_data, tiket_data, refund_data, kritiksaran_data, tikethilang_data]
+                for i in data:
+                    print(i, end=" | ")
+                    print("\n")
+
+            else:
+                print("Command tidak dikenali, gunakan \"help\" untuk bantuan")
