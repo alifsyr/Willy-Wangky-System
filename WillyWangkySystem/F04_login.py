f6136c0c2d1e88a183da5ef86ce8b97958b10f72
rayendito <56763816+rayendito@users.noreply.github.com>
2020-04-26 01:15:43 +0700

    Update F04_login.py

--- WillyWangkySystem/F04_login.py
+++ WillyWangkySystem/F04_login.py
@@ -1,22 +1,22 @@
 def login(user_data):
         
         paswValid = False
-
-
-        while not(paswValid):
-                uname = input('Masukkan username: ')
-                pasw = input('Masukkan password: ')
-                
-                for i in (user_data):
-                        if i[6] != 'Saldo':
-                                if i[3] == uname:
-                                    if i[4] == pasw:
+        uname = input('Masukkan username: ')
+        pasw = input('Masukkan password: ')
+        dummy = [" $NOUSER", " %NOUSER", " $NOUSER", " $NOUSER", " $NOUSER", "notLoggedIn", " $NOUSER"]    
+        for i in (user_data):
+                if i[6] != 'Saldo':
+                        if i[3] == uname:
+                                if i[4] == pasw:
                                         paswValid = True
                                         nama = i[0]
                                         currentUser = i
                     
-                if(paswValid):
-                        print("Selamat bersenang-senang, "+nama)
-                        return(currentUser) #untuk indikasi login berhasil
-                else:
-                        print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi!")
+        if(paswValid):
+                print()
+                print("Selamat bersenang-senang, "+nama)
+                return(currentUser) #untuk indikasi login berhasil
+        else:
+                print()
+                print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi!")
+                return(dummy)
