3870b936c0aca827510f44b3a3514d09b1760c62
Nafa-Mumtaz <57821989+Nafa-Mumtaz@users.noreply.github.com>
2020-04-25 12:21:23 +0700

    Update F12_addride.py

--- WillyWangkySystem/F12_addride.py
+++ WillyWangkySystem/F12_addride.py
@@ -8,7 +8,9 @@ def addRide(wahana_data,currentUser):
         Nama_Wahana = input("Masukkan nama wahana: ")
         Harga_Tiket = input("Masukkan harga tiket: ")
         Batasan_Umur = input("Batasan umur: ")
-        Batasan_Tinggi = input("Batasan tinggi: ")
+        Batasan_Tinggi = input("Batasan tinggi badan: ")
+        print()
         WahanaBaru = [[str(ID_Wahana),  str(Nama_Wahana), str(Harga_Tiket), str(Batasan_Umur), str(Batasan_Tinggi)]]
+        print("Info wahana telah ditambahkan!"
         wahana_data = wahana_data + WahanaBaru
         return wahana_data
