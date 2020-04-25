3eb9df3bae5c196c6c62e4af5ea35428bc97c4d8
Nafa-Mumtaz <57821989+Nafa-Mumtaz@users.noreply.github.com>
2020-04-25 12:58:46 +0700

    Update main.py

--- WillyWangkySystem/main.py
+++ WillyWangkySystem/main.py
@@ -1,7 +1,7 @@
 # Tugas Besar IF1210 Dasar Pemrograman
 # Kelompok X
 
-import F01_loadfile, F02_savefile, F03_signup, F04_login, F05_caripemain, F06_filterrides, F07_belitiket, F08_penggunaan, F09_refund, F11_accessfeedback, F13_topup, F14_ridehistory, F16_exit, B04_lostticket
+import F01_loadfile, F02_savefile, F03_signup, F04_login, F05_caripemain, F06_filterrides, F07_belitiket, F08_penggunaan, F09_refund, F10_givefeedback, F11_accessfeedback, F12_addride, F13_topup, F14_ridehistory, F16_exit, B04_lostticket
 '''
 Zachrandika Alif Syahrzea
 I Gede Govindabhakta
