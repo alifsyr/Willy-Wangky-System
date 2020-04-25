c0cd23b82f5a3345279c6717098d5c0fedcebe7d
Nafa-Mumtaz <57821989+Nafa-Mumtaz@users.noreply.github.com>
2020-04-25 12:25:56 +0700

    Update F14_ridehistory.py

--- WillyWangkySystem/F14_ridehistory.py
+++ WillyWangkySystem/F14_ridehistory.py
@@ -1,6 +1,9 @@
 def rideHistory(penggunaan_data):
-    ID = input("Masukkan ID wahana: ")
-    print("Riwayat:")
-    for i in (penggunaan_data):
-        if ID == i[2]:
-            print(str(i[1]) + " | " + str(i[0]) + " | " + str(i[3]))
+    if (currentUser[5] != "Admin"):
+        print("Anda tidak mendapat izin untuk mengakses fungsi ini.")
+    else:
+        ID = input("Masukkan ID wahana: ")
+        print("Riwayat:")
+        for i in (penggunaan_data):
+            if ID == i[2]:
+                print(str(i[1]) + " | " + str(i[0]) + " | " + str(i[3]))
