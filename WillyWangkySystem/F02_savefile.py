5347ae6d32491d2f2e493f80f4f0815b9af2e9e1
preman.id <alifsyr@gmail.com>
2020-04-27 01:07:27 +0700

    Update F02_savefile.py

--- WillyWangkySystem/F02_savefile.py
+++ WillyWangkySystem/F02_savefile.py
@@ -3,13 +3,13 @@ import csv
 
 def save(data, names):
     import modules
-    for i in range(modules.panjang(names)):
-        name = str(input("Masukkan nama file " + str(names[i]) + " :"))
-        writeFile(name, data[i])
+    for i in range(modules.panjang(names)):     # Melakukan looping berdasarkan panjang array names
+        name = str(input("Masukkan nama file " + str(names[i]) + " :"))     # input nama file csv
+        writeFile(name, data[i])    # Memanggil fungsi writeFile
         
 def writeFile(namaFile, arrayData):
-    datafile = open(namaFile, "w")
-    writer = csv.writer(datafile, delimiter=',')
+    datafile = open(namaFile, "w")      # Membuka file dari hasil input name pada fungsi save dengan fungsi open dan menggunakan metode w untuk menulis 
+    writer = csv.writer(datafile, delimiter=',')    
 
     for row in arrayData:
         writer.writerow(row)
