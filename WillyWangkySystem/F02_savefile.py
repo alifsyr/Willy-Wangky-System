8061aff77489acbdb0dcbbc32bd8385ea92089e3
preman.id <alifsyr@gmail.com>
2020-04-27 15:41:11 +0700

    Update F02_savefile.py

--- WillyWangkySystem/F02_savefile.py
+++ WillyWangkySystem/F02_savefile.py
@@ -9,6 +9,6 @@ def save(data, names):
         
 def writeFile(namaFile, arrayData):
     with open(namafile, mode='w',newline='') as csvfile:      # Membuka file dari hasil input name pada fungsi save dengan fungsi open dan menggunakan metode w untuk menulis 
-    writer = csv.writer(csvfile)    
-    for row in arrayData:
-        writer.writerow(row)
+        writer = csv.writer(csvfile)
+        for row in arrayData:
+            writer.writerow(row)
