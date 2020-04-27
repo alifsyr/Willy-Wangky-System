5afe5d5ae79446053b9fe1b570dfe450bb402626
VegetarianRacoon <igovinda100@gmail.com>
2020-04-27 23:29:17 +0700

    B01 READY?

--- WillyWangkySystem/B01_hash.py
+++ WillyWangkySystem/B01_hash.py
@@ -1,10 +1,35 @@
 # Uses PBKDF2 Hash. Requires passlib
 # Install with pip install passlib
 
-import os, hashlib
+import hashlib, binascii, os # Digunakan untuk generate angka random
 
-def hashThis(inputPassword):
-    salt = os.urandom(32)
+def hashPassword(password):
+    # Membuat salt berdasarkan nomor random yang diubah menjadi ASCII
+    salt = hashlib.sha256(os.urandom(32)).hexdigest().encode('ascii')
 
-    key = hashlib.pbkdf2_hmac('sha256', inputPassword.encode("ascii"), salt, 100000, dklen=128)
-    return 
+    # Hash password dengan PBKDF2
+    key = hashlib.pbkdf2_hmac("sha512", password.encode('ascii'), salt, 100000)
+
+    # Mengubah hash menjadi bentuk hexadesimal
+    key = binascii.hexlify(key) 
+
+    # Memberi output password yang sudah dihash dan saltnya dalam ASCII
+    return (salt + key).decode('ascii')
+
+def verifyPassword(password, inputPass):
+
+    # Salt
+    salt = password[:64]
+
+    # Menghilangkan salt dari password
+    password = password[64:]
+
+    # Hash input password. 
+    hashedInput = hashlib.pbkdf2_hmac('sha512', inputPass.encode('ascii'), salt.encode('ascii'), 100000) 
+
+    # Mengubah hashed input menjadi benduk hexadesimal
+    hashedInput = binascii.hexlify(hashedInput).decode('ascii') 
+
+    return password == hashedInput
+
+
