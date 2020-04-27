04526c3bb024f3bd7f4bba3460c00de70afbe7b1
preman.id <alifsyr@gmail.com>
2020-04-27 15:42:30 +0700

    Update F02_savefile.py

--- WillyWangkySystem/F02_savefile.py
+++ WillyWangkySystem/F02_savefile.py
@@ -8,7 +8,7 @@ def save(data, names):
         writeFile(name, data[i])    # Memanggil fungsi writeFile
         
 def writeFile(namaFile, arrayData):
-    with open(namafile, mode='w',newline='') as csvfile:      # Membuka file dari hasil input name pada fungsi save dengan fungsi open dan menggunakan metode w untuk menulis 
+    with open(namaFile, mode='w',newline='') as csvfile:      # Membuka file dari hasil input name pada fungsi save dengan fungsi open dan menggunakan metode w untuk menulis 
         writer = csv.writer(csvfile)
         for row in arrayData:
             writer.writerow(row)
