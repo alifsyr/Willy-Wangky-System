d93b222ffffd3ac8d9551d59daebce471422aa45
rayendito <56763816+rayendito@users.noreply.github.com>
2020-04-27 15:55:39 +0700

    komen

--- WillyWangkySystem/F13_topup.py
+++ WillyWangkySystem/F13_topup.py
@@ -1,12 +1,19 @@
 def topup(user_data):
+
+    #input username dan jumlah yang ingin ditopup
     uname = input('Masukkan username: ')
     tambah = int(input('Masukkan saldo yang di-top up: '))
+
+    #iterasi mencari user
     for i in (user_data):
         if i[6] != 'Saldo':
             if i[3] == uname:
+                #penyimpanan variabel nama untuk dicetak, dan penambahan saldo
                 nama = i[0]
                 i[6] = int(i[6]) + tambah
                 saldo = i[6]
+    
+    #cetak berhasil
     print()
     print("Top up berhasil. Saldo "+nama+" bertambah menjadi "+str(saldo))
 
