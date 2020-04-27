6d173249d8acdccd8db8dae729da69f33a79c2fd
preman.id <alifsyr@gmail.com>
2020-04-27 21:09:36 +0700

    Update B02_upgradegold.py

--- WillyWangkySystem/B02_upgradegold.py
+++ WillyWangkySystem/B02_upgradegold.py
@@ -1,14 +1,18 @@
 import csv
 
-def upgradegold(user_data):    
-    change_acc = input("Masukkan username yang ingin di-upgrade: ")     # Memberikan input username pemain
-    for i in (user_data) :      # Melakukan looping pada matrix user_data
-        if i[3] == change_acc:      # jika username tersedia
-            if i[6] != "Saldo":       # Untuk memisahkan nama kolom dengan data
-                if int(i[6]) >= 100000:     # Jika saldo lebih atau sama dengan 100.000   
-                    int(i[6]) - 100000
-                    i[5] = ("Gold")     # Role diubah menjadi Gold
-                    print("Akun Anda telah diupgrade.")
-                    return user_data
-                else:
-                    print("Maaf saldo anda tidak cukup.")
+def upgradegold(user_data,currentUser):    
+    if currentUser[5] == "Admin":
+        change_acc = input("Masukkan username yang ingin di-upgrade: ")     # Memberikan input username pemain
+        for i in (user_data) :      # Melakukan looping pada matrix user_data
+            if i[3] == change_acc:      # jika username tersedia
+                if i[6] != "Saldo":       # Untuk memisahkan nama kolom dengan data
+                    if int(i[6]) >= 100000:     # Jika saldo lebih atau sama dengan 100.000   
+                        newSaldo = int(i[6]) - 100000
+                        i[6] = str(newSaldo) 
+                        i[5] = ("Gold")     # Role diubah menjadi Gold
+                        print("Akun Anda telah diupgrade.")
+                        return user_data
+                    else:
+                        print("Maaf saldo anda tidak cukup.")
+    else:
+        print("Maaf, anda bukan Admin.")                    
